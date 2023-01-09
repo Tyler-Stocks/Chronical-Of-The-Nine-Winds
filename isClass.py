86e68d0e80c559bac1a11651fe993be110ede2a1
Tyler-Stocks <115584700+Tyler-Stocks@users.noreply.github.com>
2023-01-09 09:54:36 -0800

    Create isClass.py

---
+++ isClass.py
@@ -0,0 +1,10 @@
+# Defines a function that states whether or not the user is a certain class
+def isClass():
+    isClass = [
+        {'name' : 'Freelancer', 'isClass' : False},
+        {'name' : 'Explorer', 'isClass' : False},
+        {'name' : 'Knight' , 'isclass' : False},
+        {'name' : 'Sage', 'isClass' : False},
+        {'name' : 'Blind Prophet', 'isClass' : False},
+    ]
+    return isClass
