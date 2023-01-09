e1ec145442b9da05adbfeff8a55f61b52aff9f05
Tyler-Stocks <115584700+Tyler-Stocks@users.noreply.github.com>
2023-01-09 09:54:57 -0800

    Create playerClass.py

---
+++ playerClass.py
@@ -0,0 +1,99 @@
+from factions import factionsList ; from baseStats import playerBaseStats
+from changeRelation import changeFactionRelation, changeFactionRelationAll
+from changeStats import changePlayerStats ; from changeCurrencyAmount import addGold
+
+# Calls the fuction listing all of the factions so that it can be edited
+factionsList()
+
+# Calls the function listing all of the player base stats so that it can be edited
+stats = playerBaseStats()
+
+def playerClass():
+    # Prints out the different options for the player class
+    print("""What class are you:
+            1. Freelancer:
+                You were born into a completely average family, growing up in an ordinary job, and thus having no special experience. As a consequence of you completely mediocre
+                birth conditions you are neither loved nor hated, by all amicable factions. Finally, your parents being neither rich nor poor, gave you a decent amount of money
+                to put into the bank.
+                Starting Gold: 500
+                Favour: All: Neutral
+            2. Explorer: 
+                You were born to a guild of explorers. You have drifted from place to place all of you life, never staying in one place for long. As such your perceptions, and 
+                agility are much increased. Due to your assoiation with the guild, those of similar mindset will welcome you more easily. However, the guild has enemies, 
+                and should you find yourself in the hands of the Lamia, you will not be so fortunate as others. Finally, the guild gave you a small starting fund to aid you on your
+                expedition to unkown lands.
+                Starting Gold: 200
+                + 5 Perception
+                + 10 Agility
+                Relations: Guild of Explorers: Friendly
+                Favour: Lamia: Hostile
+            3. Knight:
+                You were born in to a family of Imperial Knights. You underwent severe martial training from a very young age. As a consequence of this you have much more 
+                strength, and stamina than the average person. However, due to your association with the crown those of criminal mind will despise you. Finally, you have managed to
+                save up your meager wage over the last 10 years to amount to a decent amount.
+                Starting Gold: 350
+                + 50 Stamina
+                + 10 Attack
+                Relations: High Imperial Army: Friendly
+                Relations: Court Of The Hight Moon: Friendly
+                Realtions: Pirates: Hostile
+                Relations: Theives: Hostile
+            4. Sage:
+                You were born as one of the few who has a special proclivity for magic, as such you were taken away from you family, and inducted into the Court of the High Moon at
+                a young age. After many years of vigiours training you intelect, and magical capabilities are stronger than ever. However, many fear you presence, and you will be
+                ignored by all factions. The Court lacks funding, and as such gave you only a meager starting wage which you have been able to save up over the years.
+                Starting Gold: 100
+                +500 magic
+                Relations: All: Ignored
+            5. Blind Prophet:
+                You were born blind, unable to see the world around you, as such your other senses have been heightend to compensate. As a result of this your chance of being 
+                ambushed is greatly reduced. You were cast out of your home at a young age, and as such you have no money saved up.
+                -10 ambush chance.
+                Relations: All: Neutral
+    """)
+
+    # Asks for the user input so that the progarm can know what class they are
+    playerClass = input("                ")
+
+    try:
+        # Handles if the user chooses the 'freelancer' class
+        if playerClass.lower() == "freelancer" or playerClass.lower() == 1:
+            # Changes the starting money
+            addGold(500)
+            # Changes the relationship to the different factions, this will later affect what in game decisions can be made
+            changeFactionRelationAll('Neutral')
+            
+            # Handles if the user chooses the 'explorer' class
+        elif playerClass.lower() == "explorer" or playerClass.lower() == 2:
+            # Changes the starting money
+            addGold(200)
+            # Changes the relationship to the different factions, this will later affect what in game decisions can be made
+            changeFactionRelation('Lamia', 'Hostile') ; changeFactionRelation('Guild of Explorers', 'Friendly')
+            # Changes the stats of the character
+            changePlayerStats(stats, 'perception', 15)
+        # Handles if the user chooses the 'knight of the imperial high throne' class
+        elif playerClass.lower() == "knight" or playerClass.lower() == 3:
+            # Changes the starting gold
+            addGold(350)
+            # Changes the relationship to the different factions, this will later affect what in game decisions can be made
+            changeFactionRelation('Hight Imperial Army', 'Friendly') ; changeFactionRelation('Court of the High Moon', 'Friendly')
+            changeFactionRelation('Pirates', 'Hostile') ; changeFactionRelation('Thieves', 'Hostile')
+            # Changes the stats of the character
+            changePlayerStats(stats, 'attack', '60')
+        # Handles if the user chooses the 'sage' class
+        elif playerClass.lower() == "Sage" or playerClass.lower() == 4:
+            # Changes the relationship to the different factions, this will later affect what in game decisions can be made
+            changeFactionRelationAll('Ignored')
+            # Changes the stats of the characters
+            changePlayerStats(stats, 'mana', 200)
+        # Handles if the user chooses the 'blind prophet' class
+        elif playerClass.lower() == "Blind Prophet" or playerClass.lower() == 5:
+            # Changes the relationship to the different factions, this will later affect what in game decisios can be made
+            changeFactionRelationAll('Neutral')
+            # Changes the stats of the character
+            changePlayerStats(stats, 'ambushChance', 10)
+        # Handles if the user inputs something incorrectly
+        else:
+            print("It appears that you have inputed something wrong, please try again.")
+    except TypeError:
+        print("It appears you have entered and integer or a float, please enter a string next time")
