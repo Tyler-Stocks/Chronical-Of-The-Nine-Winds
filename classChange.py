48861c5ffc2a88ed187d1a75f4033a3265de9c0d
Tyler-Stocks <tylerjstocks@gmail.com>
2023-01-09 15:41:10 -0800

    Update classChange.py
    Function to change the class of the player.

--- classChange.py
+++ classChange.py
@@ -1,1 +1,12 @@
-
+from isClass import isClass
+def changeClassStatus(className, newValue):
+    isClass()  # Call the isClass function to get the current list of classes
+  
+    # Iterate through the list of classes
+    for cls in isClass:
+        # If the name of the class matches the one passed in as an argument
+        if cls['name'] == className:
+        # Update the value of 'isClass' to the new value passed in as an argument
+            cls['isClass'] = newValue
+        # Exit the loop
+        break  
